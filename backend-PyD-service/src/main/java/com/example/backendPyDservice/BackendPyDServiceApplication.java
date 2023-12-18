02fafb7f595af790216edcb96bdcaa3aa20db833
Jorge Morales <jorge.morales.r@usach.cl>
2023-12-18 04:27:35 -0300

    cambios

--- backend-PyD-service/src/main/java/com/example/backendPyDservice/BackendPyDServiceApplication.java
+++ backend-PyD-service/src/main/java/com/example/backendPyDservice/BackendPyDServiceApplication.java
@@ -2,10 +2,10 @@ package com.example.backendPyDservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 @SpringBootApplication
-@EnableEurekaClient
+
 public class BackendPyDServiceApplication {
 
 	public static void main(String[] args) {
--- backend-profesores-service/src/main/java/com/example/backendprofesoresservice/BackendProfesoresServiceApplication.java
+++ backend-profesores-service/src/main/java/com/example/backendprofesoresservice/BackendProfesoresServiceApplication.java
@@ -2,10 +2,10 @@ package com.example.backendprofesoresservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 @SpringBootApplication
-@EnableEurekaClient
+
 public class BackendProfesoresServiceApplication {
 
 	public static void main(String[] args) {
